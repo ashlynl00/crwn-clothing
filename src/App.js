9df590421e70022786eb1562e1cdd86f3913213d
Ashlyn Lanaville <ashlyn@downing.us>
2022-06-29 15:11:27 -0700

    added category components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import './styles/categories.styles.scss';
+import CategoriesContainer from './components/categories-container/categories-container.component';
 
 const App = () => {
 
-  const categories = [
-    {
-      id: 1,
-      title: 'hats'
-    },
-    {
-      id: 1,
-      title: 'jackets'
-    },
-    {
-      id: 1,
-      title: 'shoes'
-    },
-    {
-      id: 1,
-      title: 'womens'
-    },
-    {
-      id: 1,
-      title: 'mens'
-    }
-  ];
-
   return (
-    <div className='categories-container'>
-    {categories.map( ({title})=> {
-      return (
-        <div className='category-container'>
-          <div className='background-img'></div>
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      )
-    })}
-    </div>
+    <CategoriesContainer>
+    </CategoriesContainer>
   );
 }
 
