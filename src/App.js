e93c7fac6f6a88354108803b50e7996b4a0e2aaf
Ashlyn Lanaville <ashlyn@downing.us>
2022-06-29 14:12:24 -0700

    added sass for categories

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
+import './styles/categories.styles.scss';
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'hats'
+    },
+    {
+      id: 1,
+      title: 'jackets'
+    },
+    {
+      id: 1,
+      title: 'shoes'
+    },
+    {
+      id: 1,
+      title: 'womens'
+    },
+    {
+      id: 1,
+      title: 'mens'
+    }
+  ];
+
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img></img> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+    {categories.map( ({title})=> {
+      return (
+        <div className='category-container'>
+          <div className='background-img'></div>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/* <img></img> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img></img> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img></img> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img></img> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      )
+    })}
     </div>
   );
 }
