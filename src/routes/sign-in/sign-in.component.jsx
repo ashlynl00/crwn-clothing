ccfbf3ee4327e6d8c3caa0866aa6c5e3902c4d7a
Ashlyn Lanaville <ashlyn@downing.us>
2022-07-20 16:19:44 -0700

    instantiated google redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,12 @@
-import { signInWithGooglePopUp, createUserDocumentAuth } from "../../utils/firebase/firebase.utils";
+import { auth, signInWithGooglePopUp, createUserDocumentAuth, signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
+import { getRedirectResult } from "firebase/auth";
+import { useEffect } from "react";
 
 const SignIn = () => {
+    useEffect( () => async() => {
+        const response = await getRedirectResult(auth);
+        console.log(response)
+    }, [])
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopUp();
@@ -12,6 +18,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In W/ Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In W/ Google Redirect</button>
         </div>
     )
 };
